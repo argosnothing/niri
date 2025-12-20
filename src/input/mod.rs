694207714e791a4740d19b52536a2e10ad0f4f4d
salivala <argosnothing@gmail.com>
2025-12-20 18:13:53 -0500

    Some progress on temporarily hidden workspaces

--- src/input/mod.rs
+++ src/input/mod.rs
@@ -8,6 +8,7 @@ use calloop::timer::{TimeoutAction, Timer};
 use input::event::gesture::GestureEventCoordinates as _;
 use niri_config::{
     Action, Bind, Binds, Config, Key, ModKey, Modifiers, MruDirection, SwitchBinds, Trigger,
+    WorkspaceReference,
 };
 use niri_ipc::LayoutSwitchTarget;
 use smithay::backend::input::{
@@ -1462,7 +1463,7 @@ impl State {
 
                     if let Some(output) = output {
                         self.niri.layout.focus_output(&output);
-                        self.niri.layout.switch_workspace(index);
+                        self.niri.layout.switch_workspace(index, true);
                         if !self.maybe_warp_cursor_to_focus_centered() {
                             self.move_cursor_to_output(&output);
                         }
@@ -1471,7 +1472,7 @@ impl State {
                         if config.borrow().input.workspace_auto_back_and_forth {
                             self.niri.layout.switch_workspace_auto_back_and_forth(index);
                         } else {
-                            self.niri.layout.switch_workspace(index);
+                            self.niri.layout.switch_workspace(index, true);
                         }
                         self.maybe_warp_cursor_to_focus();
                     }
@@ -2376,7 +2377,7 @@ impl State {
             }
             Action::ToggleWorkspaceVisibility(workspace_name) => {
                 self.niri.layout.toggle_workspace_visibility(workspace_name);
-            },
+            }
         }
     }
 
